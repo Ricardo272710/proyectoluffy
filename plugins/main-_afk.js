3425644499e5e75bf71bd70d0828cd02dd3e9cc7
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-25 22:55:58 -0500

    Update main-_afk.js

--- plugins/main-_afk.js
+++ plugins/main-_afk.js
@@ -1,30 +1,22 @@
-let handler = m => m 
-handler.before = async function (m, { text, args, usedPrefix, command, conn } ) {
-let user = global.db.data.users[m.sender]
+export function before(m) {
+const user = global.db.data.users[m.sender];
 if (user.afk > -1) {
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}✴️ *A F K* ✴️
-*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-*@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
-
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
-user.afk = -1
-user.afkReason = ''
+conn.reply(m.chat, `🚩 Dejastes De Estar Inactivo\n${user.afkReason ? 'Motivo De La Inactividad: ' + user.afkReason : ''}\n\n*Tiempo Inactivo: ${(new Date - user.afk).toTimeString()}*`, m, rcanal)
+user.afk = -1;
+user.afkReason = '';
 }
-let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
-for (let jid of jids) {
-let user = global.db.data.users[jid]
-if (!user)
-continue
-let afkTime = user.afk
-if (!afkTime || afkTime < 0)
-continue
-let reason = user.afkReason || ''
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
-*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
-
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
+const jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])];
+for (const jid of jids) {
+const user = global.db.data.users[jid];
+if (!user) {
+continue;
 }
-return true
+const afkTime = user.afk;
+if (!afkTime || afkTime < 0) {
+continue;
 }
-export default handler;
+const reason = user.afkReason || '';
+conn.reply(m.chat, `🚩 *El Usuario Esta Inactivo No Lo Etiquetes*`, m, rcanal)
+}
+return true;
+}
